f5e39eaa7b66578fd8066fcf49d01b515c5c159f
Shuto Mori <shushurain0@gmail.com>
2021-09-04 13:35:45 +0900

    [update] Queryset about CourseListView

--- videos/views.py
+++ videos/views.py
@@ -21,7 +21,7 @@ class CourseListView(ListView):
 
     def get_queryset(self):
         # Viewで表示するクエリセットを取得
-        queryset = self.model.objects.prefetch_related('videos')
+        queryset = self.model.objects.prefetch_related('videos').exclude(videos__isnull=True)
         return queryset
 
 
