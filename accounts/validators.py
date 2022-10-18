7db21acd2411b210f35b99bf1687233fcb4bc5f7
unknown <shushurain0@gmail.com>
2022-10-18 22:26:42 +0900

    [fix] accounts/validators.py
    パスワードのバリデータのいくつかが最初の１文字で判断していた問題を修正。

--- accounts/validators.py
+++ accounts/validators.py
@@ -26,7 +26,7 @@ class MaximumLengthValidator:
 # 半角英数字以外を含むものをはじく
 class AlphanumericValidator:
     def validate(self, password, user=None):
-        if not re.match('^[a-z0-9]+$', password):
+        if not re.search('^[a-z0-9]+$', password):
             raise ValidationError(
                 _("このパスワードには半角英数字以外が含まれています。"),
                 code='password_no_alphanumeric'
@@ -39,7 +39,7 @@ class AlphanumericValidator:
 # a-zを含まないものをはじく
 class LowercaseValidator:
     def validate(self, password, user=None):
-        if not re.match('[a-z]', password):
+        if not re.search('[a-z]', password):
             raise ValidationError(
                 _("このパスワードには1文字も小文字のa-zが含まれていません。"),
                 code='password_no_lower'
@@ -52,7 +52,7 @@ class LowercaseValidator:
 # 数字を含まないものをはじく
 class NumberValidator:
     def validate(self, password, user=None):
-        if not re.match('\d', password):
+        if not re.search('\d', password):
             raise ValidationError(
                 _("このパスワードには1文字も数字が含まれていません。"),
                 code='password_no_number',
